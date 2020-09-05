9fa10077a2e2ef026ec02582c8ee466aa7c60cf0
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-05 02:00:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  name_hash.each do |key|
+    new_key = key
+      name_hash.do |key2|
+        if key > key2
+          key = key2
+        end
+      end
+    end
+      
+  end
 
 end
